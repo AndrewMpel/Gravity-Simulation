3e40032e0881ac531683db3491b75274a15bc4f7
AndrewMpel <dit22138@office365.uop.gr>
2026-04-23 00:10:23 +0300

    Added Planets all the way to jupiter, added zoom and pan using the mouse wheel and the right click and z key to reset

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import pygame as pyg
 import math
+from pygame import key, K_z
+from collections import deque
 from pygame.math import Vector2
 
 pyg.init()
@@ -8,9 +10,11 @@ clock = pyg.time.Clock()
 
 spaceImg = pyg.image.load("media/space.png").convert()
 
-scale = 1 / 500000000
+scale = 1 / 600000000
 
 G = 6.67430 * math.pow(10,-11)
+
+zoom = 1
 
 class Body:
     def __init__(self, pos, mass, radius, vel, color):
@@ -20,11 +24,12 @@ class Body:
         self.radius = radius
         self.color = color
 
-    def draw(self, surface):
-        screen_x = self.pos.x * scale + (surface.get_width() // 2)
-        screen_y = self.pos.y * scale + (surface.get_height() // 2)
+    def draw(self, surface,zoom,offset):
+        screen_x = self.pos.x * scale *zoom+ (surface.get_width() // 2) + offset.x
+        screen_y = self.pos.y * scale * zoom+ (surface.get_height() // 2) + offset.y
 
-        pyg.draw.circle(surface, self.color, (int(screen_x), int(screen_y)), self.radius)
+        display_radius = max(1, int(self.radius * zoom))
+        pyg.draw.circle(surface, self.color, (int(screen_x), int(screen_y)), display_radius)
 
     def update(self, dt):
         self.pos += self.vel * dt
@@ -41,19 +46,46 @@ class Body:
     def acceleration(self , body: Body):
         return self.force(body)/ self.mass
 
-sun = Body((0, 0), 1.98e30, 20, (0, 0), (255, 255, 0))  # Κίτρινος Ήλιος
-earth = Body((150_000_000_000, 0), 1.5e11, 8, (0, 30_000), (100, 150, 255))  # Γαλάζια Γη
+sun = Body((0, 0), 1.98e30, 30, (0, 0), (255, 255, 0))  # Κίτρινος Ήλιος
+earth = Body((150_000_000_000, 0), 5.97e24, 8, (0, 30_000), (100, 150, 255))
+mercury = Body((57_900_000_000,0),3.30e23,3,(0,47_400),(169, 169, 169))
+venus = Body((108_000_000_000,0),4.87e24,8,(0,35_000),(255, 198, 73))
+mars = Body((228_000_000_000,0),6.39e23,6,(0,24_070),(226, 123, 88))
+jupiter = Body((778_000_000_000,0),6.39e23,18,(0,13_070),(196, 156, 126))
 
-bodies = [sun, earth]
+bodies = [sun, mercury,venus,earth,mars,jupiter]
 running = True
+offset = Vector2(0, 0)
+dragging = False
+last_mouse_pos = Vector2(0, 0)
 while running:
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
             running = False
 
-    screen.blit(spaceImg, (0, 0))
+        if event.type == pyg.MOUSEWHEEL:
+            if event.y > 0:
+                zoom *= 1.1
+            elif event.y < 0:
+                zoom /= 1.1
+        if event.type == pyg.MOUSEBUTTONDOWN:
+            if event.button == 3:
+                dragging = True
+                last_mouse_pos = Vector2(event.pos)
 
-    dt = 20000  # Το χρονικό βήμα (100 δευτερόλεπτα ανά frame)
+        if event.type == pyg.MOUSEBUTTONUP:
+            if event.button == 3: dragging = False
+
+        if event.type == pyg.MOUSEMOTION and dragging:
+            new_mouse_pos = Vector2(event.pos)
+            offset += new_mouse_pos - last_mouse_pos
+            last_mouse_pos = new_mouse_pos
+        if event.type == pyg.KEYDOWN:
+            if event.key == K_z:
+                offset = Vector2(0, 0)
+                zoom = 1
+
+    dt = 20000
 
     for b1 in bodies:
         total_accel = pyg.Vector2(0, 0)
@@ -67,7 +99,7 @@ while running:
     screen.blit(spaceImg, (0, 0))
 
     for b in bodies:
-        b.draw(screen)
+        b.draw(screen,zoom,offset)
 
     pyg.display.flip()  # Ενημέρωση της οθόνης
     clock.tick(60)  # Κλείδωμα στα 60 FPS για σταθερή φυσική
