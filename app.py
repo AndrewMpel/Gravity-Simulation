324d9e96d15bf1a800ee48b35a40cdc98e940ffc
AndrewMpel <dit22138@office365.uop.gr>
2026-04-22 17:01:28 +0300

    Set up pygame library and made game loop

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
-print("Hello world")
+import pygame as pyg
+
+pyg.init()
+screen = pyg.display.set_mode((1080, 740))
+clock = pyg.time.Clock()
+
+spaceImg = pyg.image.load("media/space.png").convert()
+
+class Body:
+    def __init__(self , pos, mass , radius , vel):
+        self.pos = pyg.Vector2(pos)
+        self.mass = mass
+        self.radius = radius
+        self.vel = vel
+
+    def draw(self):
+        pass
+        # pyg.draw.circle(radius=self.radius,center=self.pos)
+
+running = True
+while running:
+    # 1. Inputs (Events)
+    for event in pyg.event.get():
+        if event.type == pyg.QUIT:
+            running = False
+    # 2. Physics Update (Εδώ θα μπει ο Runge-Kutta σου!)
+
+    # 3. Drawing
+    screen.blit(spaceImg, (0, 0))
+
+    # Εδώ θα σχεδιάζεις τα σώματα (π.χ. pyg.draw.circle...)
+
+    pyg.display.flip()  # Ενημέρωση της οθόνης
+    clock.tick(60)  # Κλείδωμα στα 60 FPS για σταθερή φυσική
+
+pyg.quit()
