c167a497dd754a025930b933523107e7c515ee46
AndrewMpel <dit22138@office365.uop.gr>
2026-04-22 17:50:48 +0300

    Added Body class to make planets , physics methods , and created a sun and an earth

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import pygame as pyg
+import math
+from pygame.math import Vector2
 
 pyg.init()
 screen = pyg.display.set_mode((1080, 740))
@@ -6,29 +8,66 @@ clock = pyg.time.Clock()
 
 spaceImg = pyg.image.load("media/space.png").convert()
 
+scale = 1 / 500000000
+
+G = 6.67430 * math.pow(10,-11)
+
 class Body:
-    def __init__(self , pos, mass , radius , vel):
+    def __init__(self, pos, mass, radius, vel, color):
         self.pos = pyg.Vector2(pos)
+        self.vel = pyg.Vector2(vel)
         self.mass = mass
         self.radius = radius
-        self.vel = vel
+        self.color = color
 
-    def draw(self):
-        pass
-        # pyg.draw.circle(radius=self.radius,center=self.pos)
+    def draw(self, surface):
+        screen_x = self.pos.x * scale + (surface.get_width() // 2)
+        screen_y = self.pos.y * scale + (surface.get_height() // 2)
 
+        pyg.draw.circle(surface, self.color, (int(screen_x), int(screen_y)), self.radius)
+
+    def update(self, dt):
+        self.pos += self.vel * dt
+
+    def distance(self , body: Body):
+        diff = self.pos - body.pos
+        return diff.magnitude()
+
+    def force(self , body: Body):
+        r = body.pos - self.pos
+        r = r.normalize()
+        F = G*((self.mass*body.mass)/math.pow(self.distance(body),2))
+        return F * r
+    def acceleration(self , body: Body):
+        return self.force(body)/ self.mass
+
+sun = Body((0, 0), 1.98e30, 20, (0, 0), (255, 255, 0))  # Κίτρινος Ήλιος
+earth = Body((150_000_000_000, 0), 1.5e11, 8, (0, 30_000), (100, 150, 255))  # Γαλάζια Γη
+
+bodies = [sun, earth]
 running = True
 while running:
-    # 1. Inputs (Events)
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
             running = False
-    # 2. Physics Update (Εδώ θα μπει ο Runge-Kutta σου!)
 
-    # 3. Drawing
     screen.blit(spaceImg, (0, 0))
 
-    # Εδώ θα σχεδιάζεις τα σώματα (π.χ. pyg.draw.circle...)
+    dt = 20000  # Το χρονικό βήμα (100 δευτερόλεπτα ανά frame)
+
+    for b1 in bodies:
+        total_accel = pyg.Vector2(0, 0)
+
+        for b2 in bodies:
+            if b1 is not b2:
+                total_accel += b1.acceleration(b2)
+        b1.vel += total_accel * dt
+        b1.update(dt)
+
+    screen.blit(spaceImg, (0, 0))
+
+    for b in bodies:
+        b.draw(screen)
 
     pyg.display.flip()  # Ενημέρωση της οθόνης
     clock.tick(60)  # Κλείδωμα στα 60 FPS για σταθερή φυσική
