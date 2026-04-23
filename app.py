82f3e5bcd767e49080c69a012fd721faf509cdcf
AndrewMpel <dit22138@office365.uop.gr>
2026-04-23 12:23:59 +0300

    Added the + and - keys to control the speed of the simulation

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pygame as pyg
 import math
-from pygame import key, K_z
+from pygame import key, K_z, K_KP_PLUS, K_KP_MINUS
 from collections import deque
 from pygame.math import Vector2
 
@@ -43,20 +43,24 @@ class Body:
         screen_x = self.pos.x * scale *zoom+ (surface.get_width() // 2) + offset.x
         screen_y = self.pos.y * scale * zoom+ (surface.get_height() // 2) + offset.y
         display_radius = max(1, int(self.radius * zoom))
-        if zoom > 0.1:
-            text_surface = my_font.render(self.name, True, (255, 255, 255))
-            screen.blit(text_surface, (int(screen_x)-12, int(screen_y)+12))
+        if 0.05 < zoom:
+            if zoom > 2:
+                text_surface = my_font.render(self.name, True, (255, 255, 255))
+                screen.blit(text_surface, (int(screen_x) - 40, int(screen_y) + 40))
+            else:
+                text_surface = my_font.render(self.name, True, (255, 255, 255))
+                screen.blit(text_surface, (int(screen_x)-12, int(screen_y)+12))
         pyg.draw.circle(surface, self.color, (int(screen_x), int(screen_y)), display_radius)
 
         self.current_hitbox_radius = max(8, int(self.radius * zoom))
         self.current_screen_pos = (int(screen_x), int(screen_y))
 
 
-    def update(self, dt):
+    def update(self, dt ,step):
         self.pos += self.vel * dt
 
         self.step_counter += 1
-        if self.step_counter >= 10:
+        if self.step_counter >= step:
             self.path.append(pyg.Vector2(self.pos))
             self.step_counter = 0
 
@@ -93,6 +97,8 @@ running = True
 offset = Vector2(0, 0)
 dragging = False
 last_mouse_pos = Vector2(0, 0)
+dt = 20000
+step = max(1,10)
 while running:
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
@@ -119,8 +125,20 @@ while running:
             if event.key == K_z:
                 offset = Vector2(0, 0)
                 zoom = 1
+                dt = 20000
+                step = 10
+                for b in bodies:
+                    b.path.clear()
+            if event.key == K_KP_PLUS:
+                if dt < 320000:
+                    dt *= 2
+                    step = max(1, step // 2)
 
-    dt = 20000
+            if event.key == K_KP_MINUS:
+                if dt > 625:
+                    dt /= 2
+                    step = min(50, step * 2)
+
     to_remove = []
     for b1 in bodies:
         total_accel = pyg.Vector2(0, 0)
@@ -136,7 +154,7 @@ while running:
                         b1.mass += b2.mass
                         to_remove.append(b2)
         b1.vel += total_accel * dt
-        b1.update(dt)
+        b1.update(dt , step)
     for b in to_remove:
         if b in bodies: bodies.remove(b)
     screen.blit(spaceImg, (0, 0))
